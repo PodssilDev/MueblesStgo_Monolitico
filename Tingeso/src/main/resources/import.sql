3d5a8befe0da99252ff15e4da1550f21c8b922c6
John <john.serrano@usach.cl>
2022-10-04 13:32:17 -0300

    FIX: import fix

--- Tingeso/src/main/resources/import.sql
+++ Tingeso/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("20.537.567-8", "SERRANO CARRASCO", "JOHN PATRICIO", "2000/10/13", "C", "2010/05/06");
 INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("20.457.671-9", "BELLO ROBLES", "FELIPE ANDRES", "1982/05/24", "B", "2015/04/18");
-INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("20.457.671-9", "ROMAN ASENJO", "PABLO ALBERTO", "1964/11/18", "A", "2018/03/30");
+INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("27.752.982-4", "ROMAN ASENJO", "PABLO ALBERTO", "1964/04/18", "A", "2021/06/30");
+INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("20.143.123-4", "OLIVA MATURANA", "CARLA ANDREA", "2001/11/18", "C", "2018/05/30");
+INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("20.914.321-1", "FORKAN CHANA", "NICOLAS IGNACIO", "2002/09/20", "B", "2016/12/01");
