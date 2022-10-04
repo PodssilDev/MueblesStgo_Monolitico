999c79174488ded74b697eae65ac63e1c9b29dae
John <john.serrano@usach.cl>
2022-10-04 13:09:30 -0300

    fix: data.txt erroneo e import sql

---
+++ Tingeso/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("20.537.567-8", "SERRANO CARRASCO", "JOHN PATRICIO", "2000/10/13", "C", "2010/05/06");
+INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("20.457.671-9", "BELLO ROBLES", "FELIPE ANDRES", "1982/05/24", "B", "2015/04/18");
+INSERT INTO empleado(rut,apellidos,nombres,fecha_nacimiento,categoria,fecha_ingreso) VALUES ("20.457.671-9", "ROMAN ASENJO", "PABLO ALBERTO", "1964/11/18", "A", "2018/03/30");
